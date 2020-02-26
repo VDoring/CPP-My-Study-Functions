43bcb03209c3da4e4cc8d2b42b7ce6f508d9b5a9
Doring <hojun.dev@gmail.com>
2020-02-26 23:11:27 +0900

    001
    C++ Hello World!

---
+++ 초보자를 위한 C++ 200제/001.cpp
@@ -0,0 +1,15 @@
+/* 초보자를 위한 C++ 200제 - 2판, 박준태 지음*/
+
+//001
+//C++ 프로그래밍 시작하기
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	cout << "Hello C++ World!" << endl;
+
+	return 0;
+}
