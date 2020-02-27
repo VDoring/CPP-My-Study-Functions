29f434b8d42b8d38f2ac0227b06a697a2457b7ee
Doring <hojun.dev@gmail.com>
2020-02-27 19:24:21 +0900

    콘솔창에 출력하기(cout, cin, endl)

---
+++ 초보자를 위한 C++ 200제/002.cpp
@@ -0,0 +1,21 @@
+//002
+//콘솔창에 출력하기(cout, cin, endl)
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int number = 0;
+
+	cin >> number;
+
+	cout << "입력한 숫자는 " << number << "입니다!" << endl;
+
+	return 0;
+}
+/*
+cout = 콘솔창에 출력
+cin = 콘솔창 통해 입력받을때 사용. (사용자 입력을 기다림)
+
+*/
