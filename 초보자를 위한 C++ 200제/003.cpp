99d9437a8ffe8a83574b0d01a4ac3d14c8cce148
Doring <hojun.dev@gmail.com>
2020-02-28 22:09:58 +0900

    변수 배우기(int)

---
+++ 초보자를 위한 C++ 200제/003.cpp
@@ -0,0 +1,14 @@
+//003
+//변수 배우기
+#include <iostream>
+using namespace std;
+
+int main() {
+	int one = 1;
+	int two = 2;
+	int sum = one + two;
+
+	cout << "1 + 2 = " << sum << endl;
+
+	return 0;
+}
