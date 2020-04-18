99a245a12dd6f76093ff4f324d13c41c0061273c
Doring <hojun.dev@gmail.com>
2020-04-18 20:45:18 +0900

    부호 연산자 이해하기(+,-)

---
+++ 초보자를 위한 C++ 200제/022.cpp
@@ -0,0 +1,20 @@
+//022
+//부호 연산자 이해하기(+,-)
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int x = 1;
+	int y = -1;
+	int z = -2;
+
+	cout << "1 + (-1) = " << x + y << endl;
+	cout << "-1 + -2 = " << y + z << endl;
+
+	return 0;
+}
+
+/*
+C++의 부호 연산자는 일반 수학에서 사용하는 의미와 똑같다.
+*/
